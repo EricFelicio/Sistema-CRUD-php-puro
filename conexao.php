321ced8cbd8687e06c86c8c30ac57202129fe291
teco <ericfaiscaextremo@gmail.com>
2021-07-30 23:58:10 -0300

    segurança a mais.

--- conexao.php
+++ conexao.php
@@ -1,9 +1,11 @@
 <?php
-//Conexão no banco de dados
+//Verificando se conseguiu conectar ao banco de dados.
 define('HOST', '127.0.0.1');
 define('USUARIO', 'root');
 define('SENHA', 'root');
 define('BD', 'apolo20_dev');
-
 $conexao = mysqli_connect(HOST, USUARIO, SENHA, BD) or die ('Não foi possivel se conectar ao banco de dados');
+if ($conexao->connect_error) {
+  die("Conexão falhou: " . $conexao->connect_error);
+}
 ?>
--- verifica_sessao.php
+++ verifica_sessao.php
@@ -1,9 +1,12 @@
 <?php
 // Inicia sessões
 session_start();
+session_cache_expire(15);
+session_name(md5('seg'.$_SERVER['REMOTE_ADDR'].$_SERVER['HTTP_USER_AGENT']));
 
 // Verifica se existe os dados da sessão de login
 if(!isset($_SESSION["usuario"]) || !isset($_SESSION["senha"])) { 
     header("Location: login.php");
 exit; }
 ?>
+
